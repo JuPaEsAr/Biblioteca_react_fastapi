4ebf34abde2224c99882d09b9fdf5c34778e7b72
Juan Pablo EA <juanp.escobar@udea.edu.co>
2026-02-26 17:24:12 -0500

    feat:create a endpoint root and mysql connection

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+# Crear instancia principal de la aplicación FastAPI.
+# 'app' será utilizada por Uvicorn para levantar el servidor.
+app = FastAPI(
+    title="API Biblioteca Personal",
+    description="Backend REST con FastAPI para gestionar libros.",
+    version="1.0.0"
+)
+
+# Endpoint de prueba (ruta raíz).
+@app.get("/")
+def root():
+    """
+    Endpoint básico que confirma que la API está funcionando.
+    """
+    return {"mensaje": "API Biblioteca funcionando correctamente"}
+
